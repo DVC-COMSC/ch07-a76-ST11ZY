87598cf8cfeb0625930a02d0df57d16533b20b1b
Your Name <you@example.com>
2022-10-13 14:04:29 -0700

    code

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 
-num1 = list(map(int, input().split()))
-num2 = list(map(int, input().split()))
+num1 = list(map(int, input().split()));
+num2 = list(map(int, input().split()));
+merged_result=[];
+if len(num1)>=len(num2):
+    merged_result=num1+num2;
+if len(num1)<len(num2):
+    merged_result=num2+num1;
+print(merged_result);
 
-# ******************************
 # Make your Code
 # ******************************
 
